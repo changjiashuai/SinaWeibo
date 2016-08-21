70e3a020510ad3274210052f503ad7b85f59576b
changjiashuai <changjiashuai@gmail.com>
2016-08-21 17:08:50 +0800

    Initial Commit

---
+++ Weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weibo
+//
+//  Created by CJS on 16/8/21.
+//  Copyright © 2016年 CJS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
